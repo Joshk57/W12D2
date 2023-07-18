aaa1eafa0d6abc8ac42031c51e092ca9d80ccf88
joshua <joshk.5793@gmail.com>
2023-07-18 14:53:38 -0400

    phase 0

--- practice-for-ch-react-authenticate-me-long-practice/frontend/src/store/index.js
+++ practice-for-ch-react-authenticate-me-long-practice/frontend/src/store/index.js
@@ -1,10 +1,11 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import session from './session'
+import {createStore, combineReducers, applyMiddleware, compose} from "redux"
+import thunk from "redux-thunk"
+
 
 const rootReducer = combineReducers({
-  session
-});
+
+})
+
 
 let enhancer;
 
@@ -17,8 +18,9 @@ if (process.env.NODE_ENV === 'production') {
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
 
+
 const configureStore = (preloadedState) => {
-  return createStore(rootReducer, preloadedState, enhancer);
-};
+    return createStore(rootReducer, preloadedState, enhancer);
+  };
 
 export default configureStore;
